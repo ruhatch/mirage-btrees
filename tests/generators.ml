a941b1841e100c05f4d1e3f395610d3c552b55f9
Rupert Horlick <ruhatch@me.com>
2016-04-22 09:26:17 +0100

    Updated generators for new version of core

--- tests/generators.ml
+++ tests/generators.ml
@@ -1,43 +1,39 @@
 open Core_kernel.Std
 open Quickcheck.Generator
 
-module Test = Core_kernel.Std.Quickcheck
-
-let test = Test.iter
-
 let addressGenerator sizeSectors =
   let upper_bound = Excl (Int64.to_int_exn sizeSectors) in
-  int_between ~lower_bound:(Incl 0) ~upper_bound
+  Int.gen_between ~lower_bound:(Incl 0) ~upper_bound
   >>| fun x -> Int64.of_int x
 
 let cstructGenerator size =
-  list ~length:(`Exactly size) char_alphanum
+  List.gen' ~length:(`Exactly size) Char.gen_alphanum
   >>| fun list -> Cstruct.of_string (String.of_char_list list)
 
 let oblockGenerator sizeSectors sectorSize =
   tuple2 (addressGenerator sizeSectors) (cstructGenerator sectorSize)
 
 let bucketGenerator sizeSectors sectorSize bucketSize =
-  list ~length:(`Exactly bucketSize) ~unique:true (addressGenerator sizeSectors)
+  List.gen' ~length:(`Exactly bucketSize) ~unique:true (addressGenerator sizeSectors)
   >>= fun addresses ->
-  list ~length:(`Exactly bucketSize) (cstructGenerator sectorSize)
+  List.gen' ~length:(`Exactly bucketSize) (cstructGenerator sectorSize)
   >>| fun cstructs ->
   List.zip_exn addresses cstructs
 
 let leafGenerator numLeaves =
   let upper_bound = Excl (Int64.to_int_exn numLeaves) in
-  int_between ~lower_bound:(Incl 0) ~upper_bound
+  Int.gen_between ~lower_bound:(Incl 0) ~upper_bound
   >>| fun x -> Int64.of_int x
 
 let pathGenerator height sizeSectors sectorSize bucketSize =
-  list ~length:(`Exactly (height + 1)) (bucketGenerator sizeSectors sectorSize bucketSize)
+  List.gen' ~length:(`Exactly (height + 1)) (bucketGenerator sizeSectors sectorSize bucketSize)
 
 let indexGenerator maxDegree =
-  int_between ~lower_bound:(Excl 0) ~upper_bound:(Incl maxDegree)
+  Int.gen_between ~lower_bound:(Excl 0) ~upper_bound:(Incl maxDegree)
 
 let pointerGenerator =
-  int_between ~lower_bound:(Incl 0) ~upper_bound:(Excl Int.max_value)
+  Int.gen_between ~lower_bound:(Incl 0) ~upper_bound:(Excl Int.max_value)
   >>| fun x -> Int64.of_int x
 
 let keyGenerator =
-  int_between ~lower_bound:(Incl 0) ~upper_bound:(Incl 0xFF)
+  Int.gen_between ~lower_bound:(Incl 0) ~upper_bound:(Incl 0xFF)
